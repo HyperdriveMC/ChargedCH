b63bcfb4d73ebcf174da99a15af1b54172d567aa
gonsawa_ <mister.bilbis@gmail.com>
2023-01-27 20:46:16 +0000

    (chore): Add Spigot dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,23 +2,46 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
+val minecraftMajorVersion: String by project
+val minecraftMinorVersion: String by project
+val spigotVersion: String by project
+val projectVersion: String by project
+
 group = "io.github.hyperdrivemc"
-version = "1.0-SNAPSHOT"
+version = projectVersion
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+    compileOnly("org.spigotmc:spigot-api:$minecraftMajorVersion.$minecraftMinorVersion-$spigotVersion")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+tasks {
+
+    test {
+        useJUnitPlatform()
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    processResources {
+        eachFile {
+            filter<org.apache.tools.ant.filters.ReplaceTokens>(mapOf(
+                "tokens" to mapOf(
+                    "version" to project.version,
+                    "api-version" to minecraftMajorVersion
+                )
+            ))
+        }
+    }
 }

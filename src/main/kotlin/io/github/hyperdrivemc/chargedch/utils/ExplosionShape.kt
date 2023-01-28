55270c8f2cd48669c8d8d3a0d655a620caef8e5f
BuildTools <unconfigured@null.spigotmc.org>
2023-01-28 14:15:37 +0000

    (fix): Remove non-cartesian faces when getting block neighbors

--- src/main/kotlin/io/github/hyperdrivemc/chargedch/utils/ExplosionShape.kt
+++ src/main/kotlin/io/github/hyperdrivemc/chargedch/utils/ExplosionShape.kt
@@ -4,15 +4,18 @@ import org.bukkit.block.BlockFace
 import org.bukkit.block.BlockState
 
 object ExplosionShape {
+
+    private val cartesianFaces = BlockFace.values().filter { it.isCartesian }
+
     fun getBorder(blockStateList: List<BlockState>): MutableSet<BlockState> {
         val borderStateList = mutableSetOf<BlockState>()
         blockStateList.forEach {
-            BlockFace.values().forEach { face ->
-                var relativeBlock = it.block.getRelative(face).state
-                if (!blockStateList.contains(relativeBlock))  { borderStateList.add(relativeBlock) }
+            cartesianFaces.forEach { face ->
+                val relativeBlock = it.block.getRelative(face).state
+                if (!it.type.isAir && !blockStateList.contains(relativeBlock)) { borderStateList.add(relativeBlock) }
             }
         }
-        borderStateList.removeIf { it.type.isAir }
         return borderStateList
     }
+
 }

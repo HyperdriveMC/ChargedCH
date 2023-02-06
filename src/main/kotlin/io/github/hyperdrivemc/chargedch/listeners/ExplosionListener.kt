f498afc93132d0fdc21aa141dbb2b377c423a54c
BuildTools <j.duquee77@gmail.com>
2023-02-06 20:02:06 +0000

    (fix): Make block explosion heal

--- src/main/kotlin/io/github/hyperdrivemc/chargedch/listeners/ExplosionListener.kt
+++ src/main/kotlin/io/github/hyperdrivemc/chargedch/listeners/ExplosionListener.kt
@@ -6,9 +6,13 @@ import io.github.hyperdrivemc.chargedch.explosion.Explosion
 import io.github.hyperdrivemc.chargedch.explosion.ExplosionManager
 import io.github.hyperdrivemc.chargedch.utils.getRegen
 import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.Block
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockExplodeEvent
 import org.bukkit.event.block.BlockPhysicsEvent
 import org.bukkit.event.entity.EntityExplodeEvent
 
@@ -16,22 +20,12 @@ class ExplosionListener : Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     fun onEntityExplode(event: EntityExplodeEvent) {
-        if (event.location.world?.getRegen() == false) return
+        handleExplosion(event.location, event.blockList())
+    }
 
-        val explosion = Explosion(event.blockList())
-        ExplosionManager.explosions.add(explosion)
-        Bukkit.getScheduler().runTaskLater(ChargedCH.INSTANCE,
-            Runnable { ExplosionManager.explosions.remove(explosion) }, 1)
-
-        val blockStateList = event.blockList().map { it.state }.toMutableList()
-        val explosionCenter = event.location
-
-        blockStateList.sortWith(compareByDescending { it.location.distanceSquared(explosionCenter) })
-        blockStateList.sortWith(compareByDescending { -it.location.y })
-
-        ExplosionHealer(blockStateList).heal()
-
-        event.yield = 0F
+    @EventHandler
+    fun onBlockExplode(event: BlockExplodeEvent) {
+        handleExplosion(event.block.location, event.blockList())
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -40,6 +34,24 @@ class ExplosionListener : Listener {
             event.isCancelled = true
             event.block.state.update()
         }
+    }
+
+    private fun handleExplosion(center: Location, blockList: MutableList<Block>) {
+        if (center.world?.getRegen() == false) return
+
+        val explosion = Explosion(blockList)
+        ExplosionManager.explosions.add(explosion)
+        Bukkit.getScheduler().runTaskLater(ChargedCH.INSTANCE,
+            Runnable { ExplosionManager.explosions.remove(explosion) }, 1)
+
+        val blockStateList = blockList.map { it.state }.toMutableList()
+
+        blockStateList.sortWith(compareByDescending { it.location.distanceSquared(center) })
+        blockStateList.sortWith(compareByDescending { -it.location.y })
+
+        ExplosionHealer(blockStateList).heal()
+
+        blockList.forEach { it.type = Material.AIR }
     }
 
 }
